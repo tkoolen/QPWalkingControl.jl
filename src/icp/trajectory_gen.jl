a27e543aa8780a4f6090eff1216cbe50f6ce94e4
Twan Koolen <koolen.twan@gmail.com>
2019-02-12 17:07:12 -0500

    Bezier ICP trajectory WIP.

--- src/icp/trajectory_gen.jl
+++ src/icp/trajectory_gen.jl
@@ -1,73 +1,107 @@
-function integrate_icp(ξ0, p::BezierCurve, ω, t)
-    exp_ω_t = exp(ω * t)
-    return ξ0 * exp_ω_t - ω * exp_ω_t * exponential_integral(p, -ω, t)
+function integrate_icp(ξ0, p::BezierCurve, ω, θ)
+    exp_ω_t = exp(ω * θ)
+    return ξ0 * exp_ω_t - ω * exp_ω_t * exponential_integral(p, -ω, θ)
 end
 
+const COP_TRAJ_DEGREE = 2
+
 struct ICPTrajectoryGenerator{T, N, O<:MOI.AbstractOptimizer, L}
+    ω::T
     model::Model{T, O}
-    cops::Vector{SVector{2, Variable}}
-    icps::Vector{SVector{2, Variable}}
+    cop_pieces::Vector{BezierCurve{COP_TRAJ_DEGREE + 1, SVector{2, Variable}}}
+    icp_knots::Vector{SVector{2, Variable}}
     Δts::Vector{T}
-    ωs::Vector{T}
     cop_polyhedra::Vector{MHRep{N, 2, T, L}}
-    preferred_cops::Vector{Vec2{T}}
+    # preferred_cops::Vector{Vec2{T}}
     initial_icp::Base.RefValue{Vec2{T}}
     final_icp::Base.RefValue{Vec2{T}}
     num_active_segments::Base.RefValue{Int}
 
-    function ICPTrajectoryGenerator{T, N}(optimizer::O, num_segments::Integer) where {T, N, O<:MOI.AbstractOptimizer}
+    function ICPTrajectoryGenerator{T, N}(optimizer::O, num_segments::Integer, ω::Number) where {T, N, O<:MOI.AbstractOptimizer}
+        # TODO:
+        # * objective function: sum of squared differences between preferred CoPs and middle control points? Or CoP evaluated at t = Delta t / 2?
+        # * switch to quadratic Bezier curves for CoP segments?
+
+        # Variables: (COP_TRAJ_DEGREE + 5) * n + 2:
+        # * CoP control points: 2 * (COP_TRAJ_DEGREE + 1) * n
+        # * ICP knots: 2 * (n + 1)
+        # Equality constraints: 6 * n + 2:
+        # * C⁰ continuity: 2 * (n - 1)
+        # * C¹ continuity: 2 * (n - 1)
+        # * ICP dynamics: 2 * n
+        # * initial ICP: 2
+        # * final ICP: 2
+        # * final ICP == final CoP: 2
+
         n = num_segments
         L = N * 2
-
         model = Model(optimizer)
-
-        # Variables
-        cops = [SVector(ntuple(i -> Variable(model), 2)) for i = 1 : n]
-        icps = [SVector(ntuple(i -> Variable(model), 2)) for i = 1 : n]
 
         # Parameters
         Δts = Parameter(model, val=zeros(T, n))
-        ωs = Parameter(model, val=zeros(T, n))
         cop_polyhedra = Parameter(model, val=[zero(MHRep{N, 2, T, L}) for i = 1 : n])
-        preferred_cops = Parameter(model, val=[zero(Vec2{T}) for i = 1 : n])
+        # preferred_cops = Parameter(model, val=[zero(Vec2{T}) for i = 1 : n])
         initial_icp = Parameter(model, val=zero(Vec2{T}))
         final_icp = Parameter(model, val=zero(Vec2{T}))
 
-        # CoP bounds
-        for i = 1 : n
-            A = @expression cop_polyhedra[i].A
-            b = @expression cop_polyhedra[i].b
-            @constraint model A * cops[i] <= b
+        # CoP trajectory
+        cop_pieces = [BezierCurve(ntuple(i -> SVector(Variable(model), Variable(model)), Val(COP_TRAJ_DEGREE + 1))) for i = 1 : n]
+
+        # Continuity for CoP trajectory
+        for i = 1 : n - 1
+            # C⁰
+            pᵢ = cop_pieces[i]
+            pᵢ₊₁ = cop_pieces[i + 1]
+            @constraint model first(pᵢ₊₁.points) == last(pᵢ.points)
+
+            # C¹. Note: clearing denominators.
+            pᵢ′ = derivative(pᵢ)
+            pᵢ₊₁′ = derivative(pᵢ₊₁)
+            # @constraint model first(pᵢ₊₁′.points) * Δts[i] == last(pᵢ′.points) * Δts[i + 1] #  # TODO: allocates. Fix missing optimization for SVector * scalar.
+            @constraint model Vector(first(pᵢ₊₁′.points)) * Δts[i] == Vector(last(pᵢ′.points)) * Δts[i + 1]
         end
 
-        # Initial ICP
-        @constraint model icps[1] == initial_icp
-
-        # ICP dynamics
+        # CoP bounds
         for i = 1 : n
-            w = @expression exp(ωs[i] * Δts[i])
-            next_icp = @expression w * icps[i] + (1 - w) * cops[i]
-            if i < n
-                @constraint model next_icp == icps[i + 1]
-            else
-                @constraint model next_icp == final_icp
+            Aᵢ = @expression cop_polyhedra[i].A
+            bᵢ = @expression cop_polyhedra[i].b
+            pᵢ = cop_pieces[i]
+            for point in pᵢ.points
+                @constraint model Aᵢ * point <= bᵢ
             end
         end
 
-        # Objective
-        objective = Parametron.LazyExpression(identity, zero(QuadraticFunction{Float64}))
+        # ICP dynamics
+        icp_knots = [SVector(Variable(model), Variable(model)) for i = 1 : n + 1]
         for i = 1 : n
-            e = @expression cops[i] - preferred_cops[i]
-            objective = @expression objective + e ⋅ e
+            @constraint model icp_knots[i] * exp(ω) - ω * exp(ω) * exponential_integral(cop_pieces[i] , -ω) == icp_knots[i + 1]
         end
-        @objective model Minimize objective
+        @constraint model first(icp_knots) == initial_icp
+        @constraint model last(icp_knots) == final_icp
+        @constraint model last(last(cop_pieces).points) == final_icp # implies that final ICP velocity is zero
+
+        # Objective
+        # objective = Parametron.LazyExpression(identity, zero(QuadraticFunction{Float64}))
+        # for i = 1 : n
+        #     pᵢ = cop_pieces[i]
+        #     for point in pᵢ.points
+        #         objective = @expression objective + point ⋅ point
+        #     end
+        # end
+        # @objective model Minimize objective
+        # for i = 1 : n
+        #     e = @expression cops[i] - preferred_cops[i]
+        #     objective = @expression objective + e ⋅ e
+        # end
+        # @objective model Minimize objective
 
         num_active_segments = Ref(0)
 
         new{T, N, O, L}(
+            ω,
             model,
-            cops, icps,
-            Δts.val[], ωs.val[], cop_polyhedra.val[], preferred_cops.val[],
+            cop_pieces, icp_knots,
+            Δts.val[], cop_polyhedra.val[], #preferred_cops.val[],
             initial_icp.val, final_icp.val,
             num_active_segments
         )
@@ -85,19 +119,19 @@ function Base.empty!(generator::ICPTrajectoryGenerator)
 end
 
 function push_segment!(generator::ICPTrajectoryGenerator,
-        Δt::Number, ω::Number, cop_polyhedron::Union{ConvexHull, HRep}, preferred_cop::SVector)
+        Δt::Number, cop_polyhedron::Union{ConvexHull, HRep})#, preferred_cop::SVector)
     i = generator.num_active_segments[] += 1
     @boundscheck i <= length(generator.Δts) || error()
     @inbounds begin
         generator.Δts[i] = Δt
-        generator.ωs[i] = ω
+        hrep = generator.cop_polyhedra[i]
         if cop_polyhedron isa HRep
-            generator.cop_polyhedra[i] = cop_polyhedron
+            hrep.A .= cop_polyhedron.A
+            hrep.b .= cop_polyhedron.b
         else
-            hrep = generator.cop_polyhedra[i]
             hrep!(hrep.A, hrep.b, cop_polyhedron)
         end
-        generator.preferred_cops[i] = preferred_cop
+        # generator.preferred_cops[i] = preferred_cop
     end
     generator
 end
@@ -147,25 +181,27 @@ function find_segment(generator::ICPTrajectoryGenerator{T}, t::Number) where T
 end
 
 function (generator::ICPTrajectoryGenerator{T})(t::Number) where T
-    t < 0 && throw(ArgumentError("Cannot evaluate at a negative time."))
-    if t < sum(generator.Δts)
-        # Find segment index and segment start time
-        i, t0 = find_segment(generator, t)
+    # TODO: reimplement
 
-        # Use (13) in Capturability-based Analysis Part 1 to find ICP ξ
-        ξ0 = initial_icp(generator, i)
-        p = cop(generator, i)
-        Δt = t - t0
-        ω = generator.ωs[i]
-        w = exp(ω * Δt)
-        ξ = w * ξ0 + (1 - w) * p
+#     t < 0 && throw(ArgumentError("Cannot evaluate at a negative time."))
+#     if t < sum(generator.Δts)
+#         # Find segment index and segment start time
+#         i, t0 = find_segment(generator, t)
 
-        # Use (12) in Capturability-based Analysis Part 1 to find ICP time derivative ξd
-        ξd = ω * (ξ - p)
-    else
-        # Hold at the final ICP
-        ξ = generator.final_icp[]
-        ξd = zero(ξ)
-    end
-    ξ, ξd
+#         # Use (13) in Capturability-based Analysis Part 1 to find ICP ξ
+#         ξ0 = initial_icp(generator, i)
+#         p = cop(generator, i)
+#         Δt = t - t0
+#         ω = generator.ωs[i]
+#         w = exp(ω * Δt)
+#         ξ = w * ξ0 + (1 - w) * p
+
+#         # Use (12) in Capturability-based Analysis Part 1 to find ICP time derivative ξd
+#         ξd = ω * (ξ - p)
+#     else
+#         # Hold at the final ICP
+#         ξ = generator.final_icp[]
+#         ξd = zero(ξ)
+#     end
+#     ξ, ξd
 end
--- test/icptrajectory.jl
+++ test/icptrajectory.jl
@@ -16,7 +16,7 @@ const MOI = MathOptInterface
 
 using PushRecovery: integrate_icp
 import PushRecovery: ICPTrajectoryGenerator, push_segment!, solve!, initial_icp, cop, find_segment
-import PushRecovery: SHRep
+import PushRecovery: HRep, SHRep
 
 using UnicodePlots
 
@@ -49,7 +49,7 @@ end
     z = 0.95
     ω = sqrt(g / z)
     num_segments = 4
-    generator = ICPTrajectoryGenerator{Float64, 4}(optimizer(), num_segments)
+    generator = ICPTrajectoryGenerator{Float64, 4}(optimizer(), num_segments, ω)
 
     foot_width = 0.15
     foot_length = 0.3
@@ -62,7 +62,7 @@ end
         SVector(-foot_width / 2,  foot_length / 2)
     )
     foot_polygon_vrep = ConvexHull{CCW}(foot_points)
-    foot_polygon_hrep = SHRep(foot_polygon_vrep)
+    foot_polygon_hrep = HRep(foot_polygon_vrep)
 
     Δt = 0.75
 
@@ -70,45 +70,45 @@ end
         SVector((i - 1) * step_length, (-1)^i * step_width)
     end
 
-    for num_active_segments in 1 : num_segments
+    for num_active_segments in 1 : 1
         empty!(generator)
         for i = 1 : num_active_segments
-            push_segment!(generator, Δt, ω, foot_polygon_hrep + foot_centers[i], foot_centers[i])
+            push_segment!(generator, Δt, foot_polygon_hrep + foot_centers[i])
         end
         generator.initial_icp[] = foot_centers[1] + SVector(0.02, 0.01)
         generator.final_icp[] = foot_centers[num_active_segments]
 
         solve!(generator)
 
-        @test initial_icp(generator, 1) ≈ generator.initial_icp[] atol=1e-4
+        # @test initial_icp(generator, 1) ≈ generator.initial_icp[] atol=1e-4
 
-        for i = 1 : num_segments
-            C = generator.cop_polyhedra[i]
-            @test cop(generator, i) ∈ C
-        end
+        # for i = 1 : num_segments
+        #     C = generator.cop_polyhedra[i]
+        #     @test cop(generator, i) ∈ C
+        # end
 
-        let t = 0.0
-            for i in 1 : num_active_segments
-                for t′ in (max(0.0, t - eps()), t, t + eps())
-                    ξ, ξd = generator(t′)
-                    @test ξ ≈ initial_icp(generator, i)
-                    i′, t0′ = find_segment(generator, t′)
-                    if t′ > t
-                        @test i′ == i
-                        @test t0′ == t
-                    elseif t′ < t
-                        @test i′ == i - 1
-                    end
-                end
-                t += generator.Δts[i]
-            end
+        # let t = 0.0
+        #     for i in 1 : num_active_segments
+        #         for t′ in (max(0.0, t - eps()), t, t + eps())
+        #             ξ, ξd = generator(t′)
+        #             @test ξ ≈ initial_icp(generator, i)
+        #             i′, t0′ = find_segment(generator, t′)
+        #             if t′ > t
+        #                 @test i′ == i
+        #                 @test t0′ == t
+        #             elseif t′ < t
+        #                 @test i′ == i - 1
+        #             end
+        #         end
+        #         t += generator.Δts[i]
+        #     end
 
-            for t′ in (t, t + eps())
-                ξ, ξd = generator(t′)
-                @test ξ ≈ generator.final_icp[] atol=1e-4
-                @test ξd ≈ SVector(0.0, 0.0) atol=1e-4
-            end
-        end
+        #     for t′ in (t, t + eps())
+        #         ξ, ξd = generator(t′)
+        #         @test ξ ≈ generator.final_icp[] atol=1e-4
+        #         @test ξd ≈ SVector(0.0, 0.0) atol=1e-4
+        #     end
+        # end
 
         allocs = @allocated solve!(generator)
         @test allocs == 0
