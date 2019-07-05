f0558772f4dfe5118d3fef08236f990c3158d2d8
Twan Koolen <koolen.twan@gmail.com>
2019-07-05 17:44:39 -0400

    Fix performance of JointDamping + other improvements.

--- src/damping.jl
+++ src/damping.jl
@@ -1,39 +1,38 @@
 using LightXML
+using RigidBodyDynamics: JointDict
+using RigidBodyDynamics.CustomCollections: SegmentedVector
 
 struct JointDamping{T}
-    coeffs::Dict{JointID, Vector{T}}
+    coeffs::SegmentedVector{JointID, T, Base.OneTo{JointID}, Vector{T}}
 end
 
 function JointDamping{T}(mechanism::Mechanism, urdf::AbstractString) where T
     xdoc = parse_file(urdf)
     xroot = LightXML.root(xdoc)
     @assert LightXML.name(xroot) == "robot"
-    coeffs = Vector{Pair{JointID, Vector{T}}}()
+    coeffs = SegmentedVector(zeros(num_velocities(mechanism)), tree_joints(mechanism), num_velocities)
     xml_joints = get_elements_by_tagname(xroot, "joint")
     for xml_joint in xml_joints
         name = attribute(xml_joint, "name")
-        try
-            joint = findjoint(mechanism, name)
-            xml_dynamics = find_element(xml_joint, "dynamics")
-            if xml_dynamics != nothing
-                xml_damping = attribute(xml_dynamics, "damping")
-                if xml_damping != nothing
-                    push!(coeffs, JointID(joint) => [parse(T, xml_damping)])
+        xml_dynamics = find_element(xml_joint, "dynamics")
+        if xml_dynamics != nothing
+            xml_damping = attribute(xml_dynamics, "damping")
+            if xml_damping != nothing
+                local joint
+                try
+                    joint = findjoint(mechanism, name)
+                catch
+                    @warn "Joint with name \"$name\" not found. Ignoring damping coefficient in URDF."
+                    continue
                 end
+                coeffs[JointID(joint)] .= parse(T, xml_damping)
             end
-        catch # TODO: be more specific
         end
     end
-    return JointDamping(Dict(coeffs))
+    return JointDamping(coeffs)
 end
 
-function (damping::JointDamping)(τ::AbstractVector, t, state::MechanismState)
-    τ .= 0
-    v = velocity(state)
-    @inbounds for (jointid, coeffs) in damping.coeffs
-        τjoint = τ[jointid]
-        vjoint = v[jointid]
-        τjoint .= .-coeffs .* vjoint
-    end
+function (damping::JointDamping)(τ::SegmentedVector{JointID}, t, state::MechanismState)
+    τ .= .-damping.coeffs .* velocity(state)
     return τ
 end
