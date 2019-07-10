3985eeb48e3d0852217f1db35a230a8112b7a362
Twan Koolen <koolen.twan@gmail.com>
2019-07-10 14:25:31 -0400

    Framed trajectories.

--- src/com_tracking/control.jl
+++ src/com_tracking/control.jl
@@ -6,9 +6,8 @@ struct PDCoMController{T, G<:PDGains, C}
 end
 
 function (controller::PDCoMController)(t, c::Point3D, cd::FreeVector3D, active_contact_points, state)
-    @framecheck c.frame cd.frame
     mass = controller.mass
     c_des, cd_des, cdd_des = controller.com_trajectory(t, Val(2))
-    ld_des = FreeVector3D(c.frame, mass * (pd(controller.gains, c.v, c_des, cd.v, cd_des) + cdd_des.v))
+    ld_des = mass * (pd(controller.gains, c, c_des, cd, cd_des) + cdd_des)
     return ld_des
 end
