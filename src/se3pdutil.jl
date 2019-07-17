caa3ea9874d6d0edc7d20d25da2fa79132b5af80
Twan Koolen <koolen.twan@gmail.com>
2019-07-17 15:28:12 -0400

    Parameter tuning.

--- src/se3pdutil.jl
+++ src/se3pdutil.jl
@@ -2,7 +2,7 @@ function init_support!(end_effector_controller::SE3PDController, tf::Number)
     # TODO: frame lookup is kind of nasty
     bodyframe = end_effector_controller.trajectory[].body
     baseframe = end_effector_controller.trajectory[].base
-    end_effector_controller.gains[] = SE3PDGains(FramePDGains(bodyframe, PDGains(0.0, 15.0)), FramePDGains(bodyframe, PDGains(0.0, 0.0)))
+    end_effector_controller.gains[] = SE3PDGains(FramePDGains(bodyframe, PDGains(0.0, 0.0)), FramePDGains(bodyframe, PDGains(0.0, 0.0)))
     angulartraj = Constant(one(Quat)) # TODO
     lineartraj = convert(BasicFootTrajectory{Float64}, SVector(0.0, 0.0, 0.0), tf)
     end_effector_controller.trajectory[] = SE3Trajectory(bodyframe, baseframe, angulartraj, lineartraj)
@@ -16,7 +16,7 @@ function init_swing!(end_effector_controller::SE3PDController, pose0::Transform3
     baseframe = end_effector_controller.trajectory[].base
     end_effector_controller.gains[] = SE3PDGains(
         FramePDGains(bodyframe, critically_damped_gains(100.)),
-        FramePDGains(bodyframe, critically_damped_gains(100.))
+        FramePDGains(bodyframe, critically_damped_gains(200.))
     )
     angulartraj = Constant(one(Quat)) # TODO
     lineartraj = BasicFootTrajectory(t0, tf, translation(pose0), Δzmid, translation(posef), zdf)
